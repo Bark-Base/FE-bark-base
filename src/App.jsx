cbd293b586a2105d15cb6c493767584300e03d8d
KDSigel <heykarl@gmail.com>
2022-02-24 14:40:25 -0800

    added LoginSignin component. form, input, state, buttons, and submit function placeholders added. logic to switch from log in to sign in added (state). css file for login/ signin page added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import LoginSignin from './components/loginSignin/loginSignin';
+
+function App() {
+  return (
+    <div>
+      <LoginSignin />
+    </div>
+  );
+}
+
+export default App;
