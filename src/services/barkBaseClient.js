cffb2a5c3663068e9752949c589dfb8f0d95d950
KDSigel <heykarl@gmail.com>
2022-02-25 11:02:49 -0800

    fixed auth routes for logging in and signing up

--- src/services/barkBaseClient.js
+++ src/services/barkBaseClient.js
@@ -1,15 +1,18 @@
 import request from 'superagent';
 
-const URL = 'https://boiling-medow-81167.herokuapp.com/api/v1'
+const URL = 'https://boiling-meadow-81167.herokuapp.com'
 
   export async function signUpUser(email, password) {
-    const { user, error } = await request.post(`${URL}/auth`).send({ email, password });
+    const { user, error } = await request.post(`${URL}/api/v1/auth`).send({ email, password });
     if (error) throw error;
     return user;
   }
   
   export async function signInUser(email, password) {
-    const { user, error } = await request.post(`${URL}/auth`).send({ email, password });
+    const { user, error } = await request.post(`${URL}/api/v1/auth/session`).send({ email, password });
     if (error) throw error;
     return user;
   }
+
+
+  // send future requests for user-related content using .withcredentials
