359249fc7439e134f80a2ee87cac8d318757059b
DanielRGilles <danryangilles@gmail.com>
2022-03-02 17:34:55 -0800

    contacts setup

--- src/views/Contacts/Contacts.jsx
+++ src/views/Contacts/Contacts.jsx
@@ -4,21 +4,35 @@ import { getContacts } from "../../services/pets";
 import "./Contacts.css";
 
 export default function Contacts() {
-    const { user } = useUser();
-    const [ contacts, setContacts ] = useState([]);
+  const { user } = useUser();
+  const [contacts, setContacts] = useState([]);
 
-    useEffect(() => {
-        async function getAllContacts() {
-           const contactFetch = await getContacts(user.ownerId)
-           setContacts(contactFetch);
-        }
-        getAllContacts();
-      
-    }, [contacts, user.ownerId]);
-    
-    return (
-        <section>
-            {user.ownerId ? contacts?.map((contact) => <p key={contact.contactId}>{contact.name && contact.type}</p>) : <h1>...LOading</h1>}
-        </section>
-    )
+  useEffect(() => {
+    async function getAllContacts() {
+      const contactFetch = await getContacts(user.ownerId);
+      setContacts(contactFetch);
+    }
+    getAllContacts();
+  }, [contacts, user.ownerId]);
+
+  return (
+    <section>
+      <div className="contacts-wrapper">
+        {user.ownerId ? (
+          contacts?.map((contact) => (
+            <div key={contact.contactId}>
+              <h1 className="contact-type">{contact.type}</h1>
+              <p className="contact-field" >
+                {contact.name}
+              </p>
+              <p className="contact-field">{contact?.phone}</p>
+              <p className="contact-field">{contact?.address}</p>
+            </div>
+          ))
+        ) : (
+          <h1>...LOading</h1>
+        )}
+      </div>
+    </section>
+  );
 }
